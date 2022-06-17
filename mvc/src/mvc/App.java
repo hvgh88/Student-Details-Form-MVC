69449acaced807b7fbf3725db41bd459da4ceafc
Harshita Vidapanakal <65297242+hvgh88@users.noreply.github.com>
2022-06-17 15:53:40 +0530

    Update App.java

--- mvc/src/mvc/App.java
+++ mvc/src/mvc/App.java
@@ -2,7 +2,7 @@ package mvc;
 public class App {
 public static void main(String[] args) {
 // Assemble all the pieces of the MVC
-	Model m = new Model("Harshita", "Vidapanakal", "harshita.vidapanakal@gmail.com", "9000658884", "9.16", "6");
+	Model m = new Model("Harshita", "Vidapanakal", "harshita.vidapanakal@gmail.com", "9000000000", "9.16", "6");
 	View v = new View("PES1UG19CS185");
 	Controller c = new Controller(m, v);
 	c.initController();
